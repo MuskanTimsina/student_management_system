f7338e58b1658930bb68ebb54e22a36acd7e89d2
Muskan Timsinagit config --global user.email muskantimsina973@gmail.comgit initgit config --global user.name Muskan <muskantimsina973@gmail.com>
2026-06-09 23:50:07 +0545

    Add User model and password hashing setup for authentication system

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from routes.student_routes import router as student_router
+from model.student import Student
+from model.user import User
 from Database import engine,Base
 app=FastAPI()
 app.include_router(student_router)
